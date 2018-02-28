420bd71b41dceabe33198a84272409dae05dfbaf
Dan  Pereda <dpereda@gmail.com>
2018-02-28 14:30:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 function add(a,b){
   return a+b
 }
-function add(a,b){
-  return a+b
+function subtract(a,b){
+  return a-b
 }
-function add(a,b){
-  return a+b
+function multipuly(a,b){
+  return a*b
 }
-function add(a,b){
-  return a+b
+function divide(a,b){
+  return a/b
 }
